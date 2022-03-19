fc9318fa8ecd65e0105bcec34f5af682cb7a0df1
nanandt <rizkyfatih007@gmail.com>
2022-03-19 15:34:15 +0700

    create handler login

--- handler/user.go
+++ handler/user.go
@@ -11,40 +11,65 @@ type userHandler struct {
 	userService user.Service
 }
 
-func NewUserHandler(userService user.Service) *userHandler{
+func NewUserHandler(userService user.Service) *userHandler {
 	return &userHandler{userService}
 }
 
-func (h *userHandler) RegisterUser(c *gin.Context){
+func (h *userHandler) RegisterUser(c *gin.Context) {
 	var input user.RegisterUserInput
 
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
 		errors := helper.FormatValidationError(err)
-		errorMessage := gin.H{"errors":errors}
-		response := helper.APIResponse("Failed to registered", http.StatusUnprocessableEntity,"error", errorMessage)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("Failed to registered", http.StatusUnprocessableEntity, "error", errorMessage)
 		c.JSON(http.StatusUnprocessableEntity, response)
 		return
 	}
 
 	newUser, err := h.userService.RegisterUser(input)
 	if err != nil {
-		response := helper.APIResponse("Failed to registered", http.StatusBadRequest,"error", nil)
+		response := helper.APIResponse("Failed to registered", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
 	formatter := user.FormatUser(newUser, "ascbakjcbakjcabsc")
 
-	response := helper.APIResponse("Account has been registered", http.StatusOK,"success", formatter)
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
 
 	c.JSON(http.StatusOK, response)
 }
 
-func (h *userHandler)Login(c *gin.Context){
+func (h *userHandler) Login(c *gin.Context) {
 	//user masukan input(email & password )
 	//input ditangkap handler
 	//mapping dri input user ke input struct
 	//input struct passing service
 	//di service matching data dgn bantuan repository
+
+	var input user.LoginInput
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedInUser, err := h.userService.Login(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+		response := helper.APIResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedInUser, "kcakcndbckn")
+
+	response := helper.APIResponse("Login Successfuly", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+
 }
