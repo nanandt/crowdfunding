462de2f5d1fe2e8d964ab59fad0786a7cc44b69b
nanandt <rizkyfatih007@gmail.com>
2022-03-22 22:31:44 +0700

    create service upload avatar

--- user/service.go
+++ user/service.go
@@ -9,6 +9,7 @@ type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
 	Login(input LoginInput) (User, error)
 	IsEmailAvailable(input CheckEmailInput) (bool, error)
+	SaveAvatar(ID int, fileLocation string) (User, error)
 }
 
 type service struct {
@@ -71,4 +72,24 @@ func (s *service) IsEmailAvailable(input CheckEmailInput) (bool, error){
 	}
 	return false, nil
 }
+
+func (s *service)SaveAvatar(ID int, fileLocation string)(User, error){
+	/*
+	- dapatkan user by id
+	- user update attribute avatar file name
+	- save perubahan avatar file name
+	 */
+	user, err := s.repository.FindByID(ID)
+	if err != nil {
+		return user, err
+	}
+	user.AvatarFileName = fileLocation
+
+	updatedUser, err := s.repository.Update(user)
+	if err != nil {
+		return updatedUser, err
+	}
+	return updatedUser, nil
+}
+
 
